2e8783382c984ebe17243c98978b8cdf30fd0d6b
Pratyush Mishra <commercialop987@gmail.com>
2026-04-13 15:12:04 +0530

    posthog created

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import posthog from 'posthog-js' // <-- 1. Add this import
 
-createRoot(document.getElementById("root")!).render(<App />);
+// <-- 2. Add this initialization block
+posthog.init('phc_xQhsiLjq6mhqTASY4WotyjQXwDcXv9ZvjEjBjgF9zSEW', {
+  api_host: 'https://us.i.posthog.com',
+  autocapture: true 
+})
+
+createRoot(document.getElementById('root')!).render(
+  <App />
+)
